67e0ef807b01aabd8b31a451aaa307c0ffb6b521
xbmc <fernetmenta@online.de>
2012-11-02 13:20:03 +0100

    player: fix rewind

--- xbmc/cores/dvdplayer/DVDPlayerVideo.cpp
+++ xbmc/cores/dvdplayer/DVDPlayerVideo.cpp
@@ -1500,7 +1500,7 @@ double CDVDPlayerVideo::GetCurrentPts()
 
   if( m_stalled )
     iRenderPts = DVD_NOPTS_VALUE;
-  else
+  else if ( m_speed == DVD_PLAYSPEED_NORMAL)
     iRenderPts = iRenderPts - max(0.0, iSleepTime);
 
   return iRenderPts;
@@ -1599,6 +1599,8 @@ int CDVDPlayerVideo::CalcDropRequirement(double pts)
   bool   bNewFrame;
   int    iSkippedDeint = 0;
   int    iBufferLevel;
+
+  m_droppingStats.m_lastPts = pts;
 
   // get decoder stats
   if (!m_pVideoCodec->GetCodecStats(iDecoderPts, iSkippedDeint, interlaced))
